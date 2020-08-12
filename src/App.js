89806f0544f7f7acbb8d6385ff4e855e51f2c757
ElioLooiz <goodwinjack111@gmail.com>
2020-08-12 15:35:33 +0300

    Part #3 [Application] useContext hook usage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import TodoList from './TodoList'
+import React, { useState, useEffect } from 'react';
+import TodoList from './TodoList';
+import { Context } from './context';
 
 export default function App() {
   // state = {
@@ -21,7 +22,7 @@ export default function App() {
   }, [])
 
   useEffect(() => {
-    // document.addEventListener('click', handleClick)
+    // document.addEventListener('click', handleClick)    
     localStorage.setItem('todos', JSON.stringify(todos));
     // return(() => {
     //   document.removeEventListener('click', handleClick);
@@ -42,22 +43,41 @@ export default function App() {
     }
   }
 
+  const removeTodo = id => {
+    setTodos(todos.filter(todo => {
+      return todo.id !== id
+    }));
+  }
+
+  const toggleTodo = id => {
+    setTodos(todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    }))
+  }
+
   return (
-    <div className="container">
-      <h1>Todo app</h1>
+    <Context.Provider value={
+      {toggleTodo, removeTodo}
+    }>
+      <div className="container">
+        <h1>Todo app</h1>
 
-      <div className="input-field">
-        <input type="text"
-          value={todoTitle}
-          onChange={event => setTodoTitle(event.target.value)}
-          onKeyPress={addTodo}
-        />
-        <label>Todo name</label>
+        <div className="input-field">
+          <input type="text"
+            value={todoTitle}
+            onChange={event => setTodoTitle(event.target.value)}
+            onKeyPress={addTodo}
+          />
+          <label>Todo name</label>
 
+        </div>
+
+        <TodoList todos={todos} />
       </div>
-
-      <TodoList todos={todos} />
-    </div>
+    </Context.Provider>
   );
 
 }
