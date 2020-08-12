1579c2ef7bc070aa8ce60fcf0b5ec2e37073fb71
ElioLooiz <goodwinjack111@gmail.com>
2020-08-12 17:23:26 +0300

    Part #4 [Application] added useReducer hook usage

--- src/App.js
+++ src/App.js
@@ -1,67 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import TodoList from './TodoList';
 import { Context } from './context';
+import reducer from './reducer';
 
 export default function App() {
-  // state = {
-  //   todos: [
-  //     {id: 1, title: 'First todo', completed: false},
-  //     {id: 2, title: 'Second todo', completed: true},
-  //   ]
-  // }
-  const [todos, setTodos] = useState([]);
+
   const [todoTitle, setTodoTitle] = useState('');
+  const [state, dispatch] = useReducer(reducer, JSON.parse(localStorage.getItem('todos')) || [] );
 
-  const handleClick = () => {
-    console.log('click');
-  }
-
-  useEffect(() => {
-    const raw = localStorage.getItem('todos') || [];
-    setTodos(JSON.parse(raw));
-  }, [])
-
-  useEffect(() => {
-    // document.addEventListener('click', handleClick)    
-    localStorage.setItem('todos', JSON.stringify(todos));
-    // return(() => {
-    //   document.removeEventListener('click', handleClick);
-    // })
-  }, [todos]);
+  useEffect(() => {  
+    localStorage.setItem('todos', JSON.stringify(state));
+  }, [state]);
 
   const addTodo = event => {
+
     if (event.key === 'Enter') {
-      setTodos([
-        ...todos,
-        {
-          id: Date.now(),
-          title: todoTitle,
-          completed: false
-        }
-      ])
+      dispatch({
+        type: 'add',
+        payload: todoTitle,
+      });
       setTodoTitle('');
     }
   }
 
-  const removeTodo = id => {
-    setTodos(todos.filter(todo => {
-      return todo.id !== id
-    }));
-  }
-
-  const toggleTodo = id => {
-    setTodos(todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-      return todo;
-    }))
-  }
-
   return (
-    <Context.Provider value={
-      {toggleTodo, removeTodo}
-    }>
+    <Context.Provider 
+    value={{dispatch}}
+    >
       <div className="container">
         <h1>Todo app</h1>
 
@@ -75,7 +40,7 @@ export default function App() {
 
         </div>
 
-        <TodoList todos={todos} />
+        <TodoList todos={state} />
       </div>
     </Context.Provider>
   );
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,9 +1,9 @@
-import React, {useState, useContext} from 'react';
+import React, {useContext, useReducer} from 'react';
 import {Context} from './context';
 
 export default function TodoItem({title, id, completed}) {
 
-  const {toggleTodo, removeTodo} = useContext(Context);
+  const {dispatch} = useContext(Context);
   const cls = ['todo'];
 
   if(completed) {
@@ -16,13 +16,19 @@ export default function TodoItem({title, id, completed}) {
         <input
           type="checkbox"
           checked={completed}
-          onChange={() => toggleTodo(id)}
+          onChange={() => dispatch({
+            type: 'toggle',
+            payload: id,
+          })}
         />
         <span>{title}</span>
-
+ 
         <i
           className="material-icons red-text"
-          onClick = {() => removeTodo(id)}
+          onClick = {() => dispatch({
+            type: 'remove',
+            payload: id,
+          })}
         >
           delete
         </i>
